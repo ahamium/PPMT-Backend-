0ddd68b6b625290f8e1554505421284979f8a489
ahamium <ahamium@naver.com>
2019-06-19 16:05:21 +0900

    react rebuild

--- src/main/java/io/agileintelligence/ppmtool/react/WebMvcConfig.java
+++ src/main/java/io/agileintelligence/ppmtool/react/WebMvcConfig.java
@@ -9,8 +9,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/static/**")
+                .addResourceLocations("classpath:/static/static/");
     }
 }
 
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,6 +1,6 @@
-<link rel="stylesheet" type="text/css" href="/static/css/CSS.css"/>
+<link rel="stylesheet" type="text/css" href="/static/static/css/CSS.css"/>
 
-<script src="/static/js/JS.js"></script>
+<script src="/static/static/js/JS.js"></script>
 
 <body>
 hello world
