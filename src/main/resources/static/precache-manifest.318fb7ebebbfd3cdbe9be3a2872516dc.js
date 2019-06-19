690aac24e0804b04ea6c796ae125e7c5a3160bd7
ahamium <ahamium@naver.com>
2019-06-19 14:16:48 +0900

    react build and put static

---
+++ src/main/resources/static/precache-manifest.318fb7ebebbfd3cdbe9be3a2872516dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f2b4589de34836ad654e28635e2618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c912dc9154fc4636ca5",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b43dda910bd77581572a",
+    "url": "/static/css/main.abdc95a3.chunk.css"
+  },
+  {
+    "revision": "6c912dc9154fc4636ca5",
+    "url": "/static/js/2.b78537cd.chunk.js"
+  },
+  {
+    "revision": "b43dda910bd77581572a",
+    "url": "/static/js/main.ca5c271a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
