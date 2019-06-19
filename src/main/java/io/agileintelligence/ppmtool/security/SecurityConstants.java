8fa8140916b7ccf3fb4b07afaf40f16eca2c67ad
ahamium <ahamium@naver.com>
2019-06-19 13:01:44 +0900

    changed token

--- src/main/java/io/agileintelligence/ppmtool/security/SecurityConstants.java
+++ src/main/java/io/agileintelligence/ppmtool/security/SecurityConstants.java
@@ -8,7 +8,7 @@ public class SecurityConstants {
 	public static final String SECRET = "SecretKeyToGenJWTs"; //TOKEN NEEDS SECRET KEY
 	public static final String TOKEN_PREFIX = "Bearer "; 
 	public static final String HEADER_STRING = "Authorization";
-	public static final long EXPIRATION_TIME = 300_000; // 300 SECONDS
+	public static final long EXPIRATION_TIME = 3000000; // 3000 SECONDS
 
 
 }
