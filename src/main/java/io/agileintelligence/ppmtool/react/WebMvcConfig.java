95ec5420e2b1b80fee239f278709d5acfc96b95b
ahamium <ahamium@naver.com>
2019-06-20 13:43:46 +0900

    react rebuild

--- src/main/java/io/agileintelligence/ppmtool/react/WebMvcConfig.java
+++ src/main/java/io/agileintelligence/ppmtool/react/WebMvcConfig.java
@@ -1,17 +1,17 @@
-package io.agileintelligence.ppmtool.react;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/static/**")
-                .addResourceLocations("classpath:/resources/static/");
-    }
-}
+//package io.agileintelligence.ppmtool.react;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/resources/static/**")
+//                .addResourceLocations("classpath:/resources/static/");
+//    }
+//}
 
 
